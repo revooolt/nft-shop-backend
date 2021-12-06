efb6e4b58e52ceffe095cb8364c3db0f660ce4ca
Gabriel Airton <gabrielrevooolt@hotmail.com>
2021-12-06 16:56:04 -0300

    feat: ensure AddNFTController return 500 if AddNFT throws

--- src/adapters/presentation/controllers/add-nft/add-nft-controller.ts
+++ src/adapters/presentation/controllers/add-nft/add-nft-controller.ts
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { NFTData } from '../../../../domain/entities/nft/nft-data'
 import { AddNFT } from '../../../../usecases/add-nft-to-nft-shop/protocols/add-nft'
 import { AddNFTResponse } from '../../../../usecases/add-nft-to-nft-shop/protocols/add-nft-response'
 import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest, ok } from '../helpers/http-helper'
+import { badRequest, ok, serverError } from '../helpers/http-helper'
 import { Controller } from '../ports/controller'
 import { HttpRequest, HttpResponse } from '../ports/http'
 
@@ -14,27 +15,31 @@ export class AddNFTController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['artistName', 'name', 'description', 'quantity', 'price']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field] && httpRequest.body[field] !== 0) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['artistName', 'name', 'description', 'quantity', 'price']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field] && httpRequest.body[field] !== 0) {
+          return badRequest(new MissingParamError(field))
+        }
       }
+      if (!httpRequest.art) {
+        return badRequest(new MissingParamError('art'))
+      }
+      const nftData: NFTData = {
+        art: httpRequest.art.filename,
+        artistName: httpRequest.body.artistName,
+        name: httpRequest.body.name,
+        description: httpRequest.body.description,
+        quantity: httpRequest.body.quantity,
+        price: httpRequest.body.price
+      }
+      const addNFTResponse: AddNFTResponse = await this.addNFT.addNFTToNFTShop(nftData)
+      if (addNFTResponse.isLeft()) {
+        return badRequest(addNFTResponse.value)
+      }
+      return ok(nftData)
+    } catch (err) {
+      return serverError()
     }
-    if (!httpRequest.art) {
-      return badRequest(new MissingParamError('art'))
-    }
-    const nftData = {
-      art: httpRequest.art.filename,
-      artistName: httpRequest.body.artistName,
-      name: httpRequest.body.name,
-      description: httpRequest.body.description,
-      quantity: httpRequest.body.quantity,
-      price: httpRequest.body.price
-    }
-    const addNFTResponse: AddNFTResponse = await this.addNFT.addNFTToNFTShop(nftData)
-    if (addNFTResponse.isLeft()) {
-      return badRequest(addNFTResponse.value)
-    }
-    return ok(nftData)
   }
 }
