1f49ea5e3c3cf17c194f89215ecb8a5dd2fb72bd
Gabriel Airton <gabrielrevooolt@hotmail.com>
2021-12-13 16:37:55 -0300

    fix: change the return type of an interface

--- src/usecases/ports/nft-repository.ts
+++ src/usecases/ports/nft-repository.ts
@@ -2,6 +2,6 @@ import { NFTData } from '../../domain/entities/nft/nft-data'
 
 export interface NFTRepository {
   findNFTByName: (name: string) => Promise<NFTData>
-  add: (nft: NFTData) => Promise<void>
+  add: (nft: NFTData) => Promise<NFTData>
   exists: (name: string) => Promise<boolean>
 }
