f6dcfa4245021f6384a651b6a0a36e315a767a9c
Gabriel Airton <gabrielrevooolt@hotmail.com>
2021-12-14 11:37:28 -0300

    fix: change the return of add method on NFTRepository interface

--- src/usecases/ports/nft-repository.ts
+++ src/usecases/ports/nft-repository.ts
@@ -2,6 +2,6 @@ import { NFTData } from '../../domain/entities/nft/nft-data'
 
 export interface NFTRepository {
   findNFTByName: (name: string) => Promise<NFTData>
-  add: (nft: NFTData) => Promise<NFTData>
+  add: (nft: NFTData) => Promise<Omit<NFTData, 'art'>>
   exists: (name: string) => Promise<boolean>
 }
