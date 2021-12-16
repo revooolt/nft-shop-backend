25d0960ebc322abd20bfe987cc340c5f15d9cec4
Gabriel Airton <gabrielrevooolt@hotmail.com>
2021-12-16 18:18:04 -0300

    feat: connect to postgresql before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+export default {
+  port: process.env.PORT || 5050
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,2 +1,11 @@
-import app from './config/app'
-app.listen(5050, () => console.log('Server running at http://localhost:5050'))
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { TypeORMHelper } from '../external/db/postgresql/orm/typeorm/helper'
+import env from './config/env'
+
+TypeORMHelper.instance.connect()
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
