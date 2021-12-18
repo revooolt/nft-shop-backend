b6c35dc5f0b564fa1eb054bf01b386534eff32f9
Gabriel Airton <gabrielrevooolt@hotmail.com>
2021-12-18 02:33:21 -0300

    feat: add static files config

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import setupStaticFiles from './static-files'
 
 const app = express()
+setupStaticFiles(app)
 setupMiddlewares(app)
 setupRoutes(app)
 export default app
